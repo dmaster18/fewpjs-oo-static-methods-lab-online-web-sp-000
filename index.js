4c27caed22aae1e5388a46b45e7824c014b27be3
David Strauch <dmaster18@gmail.com>
2020-09-03 16:54:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ class Formatter {
   }
 
   static sanitize(string) {
-    let sanitizedString = string.replace(/[^A-Za-z0-9-']+/g, ' ');
+    let sanitizedString = string.replace(/[^A-Za-z0-9-']+/g, '');
     return sanitizedString;
   }
 
   static titleize(string) {
     let sentenceArray = string.split(" ");
     function capitalizeFirstWord(word) {
-      if (word === "a" || word === "an" || word === "but" || word === "of" || word === "and" || word === "for" || word === "at" || word === "by" || word === "from") {
+      if (word === "a" || word === "an" || word === "but" || word === "of" || word === "and" || word === "for" || word === "at" || word === "by" || word === "from" || word === "the") {
         return word;
       } else {
         let capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
